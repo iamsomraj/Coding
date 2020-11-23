59f0b25323a0f051467743d2e006b1b4c74a69f0
iamsomraj <iamsomraj@gmail.com>
2020-11-23 17:12:50 +0530

    Stack using node

---
+++ Stack/App.java
@@ -0,0 +1,68 @@
+
+class Node {
+  int data;
+  Node next;
+
+  Node(int data) {
+
+    this.data = data;
+    this.next = null;
+
+  }
+}
+
+class Stack {
+  Node top;
+  int length = 0;
+  int max = 5;
+
+  Stack(int data) {
+    Node newNode = new Node(data);
+    this.top = newNode;
+    this.length++;
+  }
+
+  void push(int data) {
+    if (this.length == max) {
+      System.out.println("Overload : Stack is full");
+      return;
+    } else {
+      Node newNode = new Node(data);
+      newNode.next = this.top;
+      this.top = newNode;
+      System.out.println("Data inserted : " + data);
+      this.length++;
+
+    }
+  }
+
+  void pop() {
+    if (this.length == 0) {
+      System.out.println("Underflow : Stack is empty");
+      return;
+    } else {
+      int dataToBeDeleted = this.top.data;
+      System.out.println("Data deleted : " + dataToBeDeleted);
+      System.out.println();
+      this.top = this.top.next;
+      this.length--;
+    }
+  }
+
+}
+
+/**
+ * App
+ */
+public class App {
+
+  public static void main(String[] args) {
+    Stack stack = new Stack(10);
+    stack.push(20);
+    stack.push(30);
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+  }
+}
